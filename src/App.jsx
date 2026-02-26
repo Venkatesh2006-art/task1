435c58db9ca32b906f7408abf8c8bd16a19d6186
Venkatesh2006-art <bymuthakavenkatesh147@gmail.com>
2026-02-26 20:19:01 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App(){
+  return(<div className='app'>
+    <h1>Hello World</h1>
+   <p>Name : B.venkatesh babu</p>
+   <p>ID  : 25000030921</p>
+   <p>Branch : CSE</p>
+   <p>College : K L Engineering College</p>
+  </div>);
+  
+}
+export default App;
